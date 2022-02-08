3568f48b45f8a5f1bbc4d0ad40a81d066e3a726d
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-02-08 16:49:41 +0200

    update: button colors

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return <nav className="bg-gradient-to-l from-gray-200 via-emerald-50 to-emerald-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
             <a href="/" className="flex self-center text-lg font-semibold whitespace-nowrap">Algo Broker</a>
-            <button onClick={toggleMenu} className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-emerald-100 focus:outline-none focus:ring-2 focus:ring-emerald-200">
+            <button onClick={toggleMenu} className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200">
                 <HiOutlineMenu size="20"/>
             </button>
             <div className="hidden w-full md:block md:w-auto">
